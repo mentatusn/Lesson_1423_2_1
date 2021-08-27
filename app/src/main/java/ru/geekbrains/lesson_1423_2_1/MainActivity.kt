d7aa01070e1803cc9dcbd2428898d6b12e8948c7
Andej Nesterenko <mentatus.n@gmail.com>
2021-08-27 21:18:50 +0300

    Поигрались с классами, их свойствами(НЕ ПОЛЯМИ!) и функциями(НЕ МЕТОДАМИ!)

--- app/src/main/java/ru/geekbrains/lesson_1423_2_1/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/lesson_1423_2_1/MainActivity.kt
@@ -1,11 +1,31 @@
 package ru.geekbrains.lesson_1423_2_1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+    var i:Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        val objTest = Test();
+        val getI = objTest.i;
+        objTest.i = 2;
+        this.i = 2;
+        this.i =try {
+            throw IllegalStateException()
+        }catch (e:IllegalStateException){
+            3
+        }
+        Log.d("mylogs","$i  sdrgrerg $i  ${objTest.i}")
     }
+}
+class Test(){
+    var i:Int = 0
+}
+
+fun test():Int{
+    var i:Int = 0
+    return i
 }
---
+++ app/src/main/java/ru/geekbrains/lesson_1423_2_1/NewClass.kt
@@ -0,0 +1,7 @@
+package ru.geekbrains.lesson_1423_2_1
+
+class NewClass {
+    fun test2(){
+        val i = test()
+    }
+}
