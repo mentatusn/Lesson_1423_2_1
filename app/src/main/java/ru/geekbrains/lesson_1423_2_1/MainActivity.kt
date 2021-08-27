4690f87eef892ae88f26814258f167f442c051a7
Andej Nesterenko <mentatus.n@gmail.com>
2021-08-28 00:14:22 +0300

    Закончини урок 1

--- app/src/main/java/ru/geekbrains/lesson_1423_2_1/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/lesson_1423_2_1/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.geekbrains.lesson_1423_2_1
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 open class MainActivity : AppCompatActivity(){
@@ -16,15 +17,21 @@ open class MainActivity : AppCompatActivity(){
         }
     }
 
-
-
     var i:Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+         /*val btn = findViewById<Button>(R.id.SHOW_ALL)
+        btn.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                TODO("Not yet implemented")
+            }
+        })*/
         val objTest = Test("Аня", "Оля")
-        Log.d("mylogs","$i  sdrgrerg $i  ${objTest.name1}")
-        Log.d("mylogs","$i  sdrgrerg $i  ${objTest.name2}")
+        //val objTest2 = objTest.copy()
+        //objTest2.name2 = objTest2.name1
+       // Log.d("mylogs","$i  sdrgrerg $i  ${objTest.name1}")
+       // Log.d("mylogs","$i  sdrgrerg $i  ${objTest.name2}")
         val getI = objTest.i;
         objTest.i = 2;
         this.i = 2;
@@ -33,10 +40,8 @@ open class MainActivity : AppCompatActivity(){
         }catch (e:IllegalStateException){
             3
         }
-
         this.i = if( objTest.i==3) {2;
             1}  else 3
-
         this.i =1
         val str = when(this.i){
             1-> "1"
@@ -44,16 +49,44 @@ open class MainActivity : AppCompatActivity(){
             int1}
             else ->1
         }
-
-        Log.d("mylogs","$str")
-
-
+       // Log.d("mylogs","$str")
         val test3 = object  {
             var i:Int = 0
             val field1 = "STATIC_STRING"
         }
         //Log.d("mylogs","  ${test3.i}")
         //Log.d("mylogs","  ${test3.field1}")
+
+
+
+        /** циклы */
+        val daysOfWeek = listOf("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday")
+
+        repeat(daysOfWeek.size){
+           // Log.d("mylogs","$it")
+           // Log.d("mylogs","${daysOfWeek[it]}")
+
+        }
+
+
+
+        for(day in daysOfWeek){
+            //Log.d("mylogs","$day")
+
+        }
+        for(i in 0..daysOfWeek.size-1){
+            //Log.d("mylogs", daysOfWeek[i])
+        }
+        for(i in 0 until daysOfWeek.size){
+            //Log.d("mylogs", daysOfWeek[i])
+        }
+        for(i in daysOfWeek.indices){
+          //  Log.d("mylogs", daysOfWeek[i])
+        }
+        for(i in daysOfWeek.size-1 downTo 0 step 1){ }
+        daysOfWeek.reversed().forEach{
+            Log.d("mylogs","$it")
+        }
 
 
     }
@@ -63,9 +96,8 @@ class Test(var name1: String, var name2: String) {
 
 }
 
-fun test(name1: String,name2: String=""):Int{
-    var i:Int = 0
-    return i
+fun test():Nothing{
+    while (true){
 
-
+    }
 }
